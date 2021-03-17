79128ab59aeed15c966d2e1495977bc015d1c878
Vasiliy Merzlykov <v.merov@yandex.ru>
2021-03-17 16:30:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React, { useState} from 'react';
+
+function DateTime(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p className="date">{props.date}</p>
+  )
+}
+
+function Video(props) {
+  return (
+    <div className="video">
+      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <UpgComponent date={props.date} />
     </div>
+  )
+}
+
+function VideoList(props) {
+  return props.list.map(item => <Video  key={item.date} url={item.url} date={item.date} />);
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2017-07-31 13:24:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2021-03-17 13:10:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2021-03-17 14:50:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2018-01-03 12:10:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2018-01-01 16:17:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2017-12-02 05:24:00'
+    },
+  ]);
+
+  return (
+    <VideoList list={list} />
   );
 }
 
-export default App;
+function DateTimePretty(Component) {
+  function NewComponent(props) {
+    let difference = (new Date().getTime() - new Date(props.date).getTime())
+    let date = difference > (24 * 60 * 60 * 1000) ? `${Math.floor(difference/(1000*60*60*24))} дней назад` : 
+    difference > (60 * 1000 * 5) ? "5 часов назад" : "12 минут назад" 
+       
+
+    return <Component {...props} date = {date}/>
+  }
+  return NewComponent;
+
+}
+
+const UpgComponent = DateTimePretty(DateTime)
